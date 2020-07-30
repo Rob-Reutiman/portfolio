ba0af89d5603e8a72c12e9a9870aefc0ce1af9dd
Robert Reutiman <rreutima@nd.edu>
2020-07-30 10:16:06 -0400

    Update welcome page with code segment

--- src/components/about.js
+++ src/components/about.js
@@ -7,20 +7,32 @@ class About extends React.Component {
     return(
       <React.Fragment>
         <Nav active={this.props.match.url}/>
-        <div className="page-content">
-          <div className='hello-world'>
-            <h1 className='center-text highlight'>Hello, World! Here's what you need to know about me.</h1>
+        <div className='page-content'>
+          <div className='header'>
+            <h1>Hello, World! Here's what you need to know about me.</h1>
           </div>
-          <p className='white hello-world'>rob_Reutiman = &#123;</p>
-          <ul className='list hello-world'>
-            <li className='highlight list-item'>&#09;origin: <span className='white welcome'>"Mill Valley, CA",</span></li>
-            <li className='highlight list-item'>&#09;education: <span className='white welcome'>"University of Notre Dame",</span></li>
-            <li className='highlight list-item'>&#09;major: <span className='white welcome'>"Computer Science",</span></li>
-            <li className='highlight list-item'>&#09;interests: <span className='white welcome'>&#91; Backpacking, Spearfishing, Video Games, Photography, Japanese Language and Culture &#93;,</span></li>
-            <li className='highlight list-item'>&#09;major: <span className='white welcome'>"Computer Science",</span></li>
-
-          </ul>
-          <p className='welcome hello-world'>&#125;</p>
+          <div className='code_box'>
+            <span>Rob_Reutiman = &#123;</span>
+            <ul className='list'>
+              <li>&#09;origin: <span>Mill Valley, CA,</span></li>
+              <li>&#09;education: <span>University of Notre Dame,</span></li>
+              <li>&#09;major: <span>Computer Science,</span></li>
+              <li>&#09;GPA: <span>3.38,</span></li>
+              <li>&#09;interests: <span>&#91; Coding, Backpacking, Spearfishing, Video Games, Photography, Japanese Language and Culture &#93;,</span></li>
+              <li>&#09;latest_work_experience: <span>&#123;
+                <ul className='list'>
+                  <li>&#09;company: <span>FamHis,</span></li>
+                  <li>&#09;role: <span>Junior Software Engineer,</span></li>
+                  <li>&#09;duration: <span>Jan - July 2020</span></li>
+                </ul>&#125;,</span>
+              </li>
+              <li>&#09;resume: <a href='reutiman_resume.pdf'>reutiman_resume.pdf</a></li>
+            </ul>
+            <span>&#125;</span>
+          </div>
+          <div className='header'>
+            <h1>Projects</h1>
+          </div>
         </div>
       </React.Fragment>
     );
