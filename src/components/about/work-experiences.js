01c87d2756e777927a4097e4653704f242233f3e
Robert Reutiman <rreutima@nd.edu>
2023-01-30 15:19:46 -0800

    Small typo fixes

--- src/components/about/work-experiences.js
+++ src/components/about/work-experiences.js
@@ -2,7 +2,7 @@ const experiences = [
   {
     company: "Pariveda Solutions", 
     role: "Software Engineer",
-    dates: "Sep 2021 -",
+    dates: "Aug 2021 -",
     bullets: [ 'Developed a full stack infrastructure automation tool for a mobile telecommunication company with Javascript, Angular, and Node.js', 'Built out a set of microservice APIs with Node.js and programmed their integrations with core app', 'Coded a testing suite using Mocha and Chai that ensured code coverage of +80%', 'Assisted in a Fortune 500 online travel company\'s transition from legacy cloud infrastructure to a next-gen in-house infrastructure', 'Reconfigured Dockerfiles, Helm charts, S3 bucket policies, and CI/CD pipelines to support new platform', 'Migrated a subset of a monolithic application’s core booking functionality to a standalone Java API to reduce app complexity and improve security', 'Wrote unit tests with JUnit to validate functionality' ]
   },
   {
